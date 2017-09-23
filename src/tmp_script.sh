18b675f8d0829d88bcbe22e89e91e7b57e400846
Emmanuel Vargas <emvargcr@gmail.com>
2017-09-23 15:45:54 -0600

    Salvo history de clase 5 + ejemplo de su

--- src/tmp_script.sh
+++ src/tmp_script.sh
@@ -28,4 +28,9 @@ then
 	echo HOLA $NOMBRE, su correo es $EMAIL
 fi
 
+echo -en "${YELLOW}Agregando configuracion de git para el usuario $NOMBRE${RESET}"
+sudo su - estudiantes -c "git config --global user.name \"$NOMBRE\""
+sudo su - estudiantes -c "git config --global user.email \"$EMAIL\""
+sudo su - estudiantes -c "git config --global core.editor vim"
+
 
